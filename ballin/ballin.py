3f3998c137d82293b4935d3415400d6c590a63f5
Kenneth Woo <kennethwoo11@gmail.com>
2023-10-14 18:35:00 -0400

    foot ball

--- ballin/ballin.py
+++ ballin/ballin.py
@@ -7,27 +7,6 @@ import datetime
 storage = {}
 previous_payloads = []
 
-def scoreboard_request():
-    # Define the API URL
-    url = "https://site.api.espn.com/apis/site/v2/sports/football/college-football/scoreboard"
-
-    # Send an HTTP GET request to the API
-    response = requests.get(url)
-
-    # Check if the request was successful (status code 200)
-    if response.status_code == 200:
-        # Parse the JSON data from the response
-        data = response.json()
-        
-        scoreboarddata = json.dumps(data, indent=4)
-
-    else:
-        # Print an error message if the request was not successful
-        print("Error: Unable to fetch data from the ESPN API")
-
-    return data
-
-#information object that will be stored in storage
 class GameInfo:
     def __init__(self, id, team1, team2, last_play, team1_score, team2_score, downDistanceText, time_qtr):
         self.id = id
@@ -39,12 +18,24 @@ class GameInfo:
         self.downDistanceText = downDistanceText
         self.time_qtr = time_qtr
 
+def scoreboard_request():
+    url = 'https://site.api.espn.com/apis/site/v2/sports/football/college-football/scoreboard'
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        
+        scoreboarddata = json.dumps(data, indent=4)
+
+    else:
+        print("Error: Unable to fetch data from the ESPN API")
+
+    return data
+
+
+
 def sb_parser(scoreboard_data):
 
-    #how many events in scoreboard data
     eventcount = len(scoreboard_data['events'])
-
-    #loop through each event
     for evc in range(eventcount):
 
         #if the event is not live, skip it
@@ -58,15 +49,11 @@ def sb_parser(scoreboard_data):
         #print(f'{team1} vs {team2}')
         sb_key = f'{id}_{team1}_{team2}'
         #print(sb_key)
-
-        #last play
         try:
             last_play = scoreboard_data['events'][evc]['competitions'][0]['situation']['lastPlay']['text']
         except:
             last_play = 'No last play available'
         #print(last_play)
-
-        #get score
         team1_score = scoreboard_data['events'][evc]['competitions'][0]['competitors'][0]['score']
         team2_score = scoreboard_data['events'][evc]['competitions'][0]['competitors'][1]['score']
         #print(f'{team1} {team1_score} - {team2} {team2_score}')
@@ -79,10 +66,7 @@ def sb_parser(scoreboard_data):
         #status type detail is time and quarter
         time_qtr = scoreboard_data['events'][evc]['status']['type']['detail']
 
-        #create GameInfo object
         game_info = GameInfo(id, team1, team2, last_play, team1_score, team2_score, downDistanceText, time_qtr)
-
-        #store GameInfo object in storage
         storage[sb_key] = game_info
 
 def info_printer(gio):
@@ -101,12 +85,7 @@ def info_printer(gio):
     previous_payloads.append(info)
     return info
 
-
-
-
-
 update_interval = 90
-
 
 #test data
 # parse scoreboard.json
